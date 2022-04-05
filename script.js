8fa712aedbea528dca9207ad3f686b4414ead7db
Eduardo Mello Martins <78046473+EduardoMMartins@users.noreply.github.com>
2022-04-05 09:10:22 -0300

    Versão 05/04/22

---
+++ script.js
@@ -0,0 +1,56 @@
+//console.clear();
+
+/*function converterMoeda() {
+  //Valores coletados dia 08/03/2022 às 11:26 am no Google
+  const valorDolar = 5.09;
+  const valorEuro = 5.55;
+  const valorIene = 0.044;
+  const valorFrancoSuico = 5.48;
+
+  var valorColetado = document.getElementById("valorColetado");
+  var valor = parseFloat(valorColetado.value);
+  var moedaSelecionada = document.getElementById("moedaSelecionada");
+  var moeda = moedaSelecionada.value;
+  var valorConvertido = 0;
+
+  switch (moeda) {
+    case "dolar":
+      console.log("Dolar");
+      valorConvertido = valor * valorDolar;
+      document.getElementById("valorConvertido").innerHTML =
+        "US$" +
+        valor.toFixed(2) +
+        " correspondem à: <br> R$" +
+        valorConvertido.toFixed(2);
+      break;
+    case "euro":
+      console.log("Euro");
+      valorConvertido = valor * valorEuro;
+      document.getElementById("valorConvertido").innerHTML =
+        "€" +
+        valor.toFixed(2) +
+        " correspondem à: <br> R$" +
+        valorConvertido.toFixed(2);
+      break;
+    case "iene":
+      console.log("Iene");
+      valorConvertido = valor * valorIene;
+      document.getElementById("valorConvertido").innerHTML =
+        "¥" +
+        valor.toFixed(2) +
+        " correspondem à: <br> R$" +
+        valorConvertido.toFixed(2);
+      break;
+    case "francoSuico":
+      console.log("Franco Suiço");
+      valorConvertido = valor * valorFrancoSuico;
+      document.getElementById("valorConvertido").innerHTML =
+        "SFr" +
+        valor.toFixed(2) +
+        " correspondem à: <br> R$" +
+        valorConvertido.toFixed(2);
+      break;
+  }
+}*/
+
+var _0x2b7c7b=_0x5c85;function _0x5c85(_0x5684fb,_0x5bfc54){var _0x77296d=_0x7729();return _0x5c85=function(_0x5c85ca,_0x2adace){_0x5c85ca=_0x5c85ca-0x125;var _0x2831ed=_0x77296d[_0x5c85ca];return _0x2831ed;},_0x5c85(_0x5684fb,_0x5bfc54);}(function(_0x505dac,_0x1917a9){var _0x3a7656=_0x5c85,_0x228aae=_0x505dac();while(!![]){try{var _0x35cb02=-parseInt(_0x3a7656(0x131))/0x1*(-parseInt(_0x3a7656(0x139))/0x2)+-parseInt(_0x3a7656(0x12f))/0x3*(parseInt(_0x3a7656(0x13a))/0x4)+-parseInt(_0x3a7656(0x130))/0x5+parseInt(_0x3a7656(0x12d))/0x6+parseInt(_0x3a7656(0x126))/0x7*(parseInt(_0x3a7656(0x133))/0x8)+-parseInt(_0x3a7656(0x128))/0x9+parseInt(_0x3a7656(0x13e))/0xa;if(_0x35cb02===_0x1917a9)break;else _0x228aae['push'](_0x228aae['shift']());}catch(_0x1a831a){_0x228aae['push'](_0x228aae['shift']());}}}(_0x7729,0x8a0bc),console[_0x2b7c7b(0x127)]());function _0x7729(){var _0x132bcb=['3898450XcEMrk','604307Vzkadj','value','88uBVqbO','euro','Euro','valorConvertido','iene','dolar','2VHDYPA','20ZRYLNz','Iene','francoSuico','valorColetado','16860560xYHYgs','getElementById','SFr','moedaSelecionada','44569Tgirxc','clear','9966861VpIHlJ','log','\x20correspondem\x20à:\x20<br>\x20R$','toFixed','innerHTML','3047610LTpnpT','Franco\x20Suiço','249468ubyfud'];_0x7729=function(){return _0x132bcb;};return _0x7729();}function converterMoeda(){var _0x43d06e=_0x2b7c7b;const _0x492b8b=5.09,_0x2293bd=5.55,_0x14656e=0.044,_0x42fa54=5.48;var _0x246354=document[_0x43d06e(0x13f)](_0x43d06e(0x13d)),_0x2020f5=parseFloat(_0x246354[_0x43d06e(0x132)]),_0x1f95e9=document['getElementById'](_0x43d06e(0x125)),_0x877a72=_0x1f95e9[_0x43d06e(0x132)],_0x96124e=0x0;switch(_0x877a72){case _0x43d06e(0x138):console[_0x43d06e(0x129)]('Dolar'),_0x96124e=_0x2020f5*_0x492b8b,document[_0x43d06e(0x13f)](_0x43d06e(0x136))[_0x43d06e(0x12c)]='US$'+_0x2020f5[_0x43d06e(0x12b)](0x2)+_0x43d06e(0x12a)+_0x96124e[_0x43d06e(0x12b)](0x2);break;case _0x43d06e(0x134):console[_0x43d06e(0x129)](_0x43d06e(0x135)),_0x96124e=_0x2020f5*_0x2293bd,document[_0x43d06e(0x13f)](_0x43d06e(0x136))[_0x43d06e(0x12c)]='€'+_0x2020f5[_0x43d06e(0x12b)](0x2)+_0x43d06e(0x12a)+_0x96124e[_0x43d06e(0x12b)](0x2);break;case _0x43d06e(0x137):console['log'](_0x43d06e(0x13b)),_0x96124e=_0x2020f5*_0x14656e,document['getElementById'](_0x43d06e(0x136))[_0x43d06e(0x12c)]='¥'+_0x2020f5[_0x43d06e(0x12b)](0x2)+_0x43d06e(0x12a)+_0x96124e[_0x43d06e(0x12b)](0x2);break;case _0x43d06e(0x13c):console[_0x43d06e(0x129)](_0x43d06e(0x12e)),_0x96124e=_0x2020f5*_0x42fa54,document[_0x43d06e(0x13f)]('valorConvertido')[_0x43d06e(0x12c)]=_0x43d06e(0x140)+_0x2020f5[_0x43d06e(0x12b)](0x2)+_0x43d06e(0x12a)+_0x96124e[_0x43d06e(0x12b)](0x2);break;}}
